98da9e221019aa6515a774f8a66f616859b352d6
Felipe Silva <feliperochasilva1987@gmail.com>
2018-12-21 16:59:08 -0200

    begining of select

--- cadastro-servico.php
+++ cadastro-servico.php
@@ -6,23 +6,33 @@ if (empty($_SESSION['start'])) {
     header("Location:login.php");
 }
 ?>
-     <div class="container">   
+     <div class="container-fluid">   
      <div id="cadastrandoone">
      <h2>Cadastrar Serviços</h2>
-     </div>                 
+     <form>
+     <select class="form-control form-control-lg">
+    <option>Selecione o Serviço</option>
+    <option>Mensalista</option>
+    <option>Diarista</option>
+    <option>Horista</option>
+    </select>    
+    </form>
+     </div>   
+     <div class="container">                 
      <form class="formCadastrando-Servico">    
      <div class="row">
      <div class="col-sm-8 col-md-10 col-lg-7">
-     <h6 class="cadastroServicoCenter">Cliente</h6><input type="text" class="form-control" disabled value="<?php echo $_SESSION['nome']; ?>">
+     <h6 class="cadastroServicoCenter">Cliente</h6><input type="text" class="form-control" readonly value="<?php echo $_SESSION['nome']; ?>">
      </div>
      <div class="col-sm-8 col-md-10 col-lg-7">
-     <h6 class="cadastroServicoCenter">Veículo</h6><input type="text" class="form-control" disabled value="<?php echo $_SESSION['veiculo']; ?>">
+     <h6 class="cadastroServicoCenter">Veículo</h6><input type="text" class="form-control" readonly value="<?php echo $_SESSION['veiculo']; ?>">
      </div>    
      <div class="col-sm-8 col-md-10 col-lg-7">
-     <h6 class="cadastroServicoCenter">Placa</h6 ><input type="text" class="form-control" disabled value="<?php echo $_SESSION['placa']; ?>">
+     <h6 class="cadastroServicoCenter">Placa</h6 ><input type="text" class="form-control" readonly value="<?php echo $_SESSION['placa']; ?>">
      </div>                
      </div>
-     </form>
-</div>   
+     </div>     
+</div>
+
 </body>
 </html>
--- servico.php
+++ servico.php
@@ -77,19 +77,19 @@ if(isset($_POST['btn_pesquisa_para_servico_placa'])){
 
                 <div class="row">
                     <div class="col-sm-8 col-md-10 col-lg-7">
-                        <input type="text" name="pesquisa1" placeholder="Resultado" class="form-control" id="input1" disabled value="<?php echo isset($cliente['nome']) ? $cliente['nome'] : '' ; //se o nome foi setado o nome vai imprimir se nao vai imprimir nome inexistente, o nome inexistente foi definido la em cima nos codigos php?>">
+                        <input type="text" name="pesquisa1" placeholder="Resultado" class="form-control" id="input1" readonly value="<?php echo isset($cliente['nome']) ? $cliente['nome'] : '' ; //se o nome foi setado o nome vai imprimir se nao vai imprimir nome inexistente, o nome inexistente foi definido la em cima nos codigos php?>">
                     </div>
                     <div class="col-sm-8 col-md-10 col-lg-7">
-                        <input type="text" name="pesquisa" placeholder="Resultado" class="form-control" id="input2" disabled value="<?php echo isset($cliente['cpf']) ? $cliente['cpf'] : '' ; //se o nome foi setado o nome vai imprimir se nao vai imprimir nome inexistente, o nome inexistente foi definido la em cima nos codigos php?>">
+                        <input type="text" name="pesquisa" placeholder="Resultado" class="form-control" id="input2" readonly value="<?php echo isset($cliente['cpf']) ? $cliente['cpf'] : '' ; //se o nome foi setado o nome vai imprimir se nao vai imprimir nome inexistente, o nome inexistente foi definido la em cima nos codigos php?>">
                     </div>
                     <div class="col-sm-8 col-md-10 col-lg-7">
-                        <input type="text" name="pesquisa" placeholder="Resultado" class="form-control" id="input3" disabled value="<?php echo isset($cliente['telefone']) ? $cliente['telefone'] : '' ; //se o nome foi setado o nome vai imprimir se nao vai imprimir nome inexistente, o nome inexistente foi definido la em cima nos codigos php?>">
+                        <input type="text" name="pesquisa" placeholder="Resultado" class="form-control" id="input3" readonly value="<?php echo isset($cliente['telefone']) ? $cliente['telefone'] : '' ; //se o nome foi setado o nome vai imprimir se nao vai imprimir nome inexistente, o nome inexistente foi definido la em cima nos codigos php?>">
                     </div>
                     <div class="col-sm-8 col-md-10 col-lg-7">
-                        <input type="text" name="pesquisa" placeholder="Resultado" class="form-control" id="input4" disabled value="<?php echo isset($cliente['veiculo']) ? $cliente['veiculo'] : '' ; //se o nome foi setado o nome vai imprimir se nao vai imprimir nome inexistente, o nome inexistente foi definido la em cima nos codigos php?>">
+                        <input type="text" name="pesquisa" placeholder="Resultado" class="form-control" id="input4" readonly value="<?php echo isset($cliente['veiculo']) ? $cliente['veiculo'] : '' ; //se o nome foi setado o nome vai imprimir se nao vai imprimir nome inexistente, o nome inexistente foi definido la em cima nos codigos php?>">
                     </div>
                     <div class="col-sm-8 col-md-10 col-lg-7">
-                        <input type="text" name="pesquisa" placeholder="Resultado" class="form-control" id="input5" disabled value="<?php echo isset($cliente['placa']) ? $cliente['placa'] : '' ; //se o nome foi setado o nome vai imprimir se nao vai imprimir nome inexistente, o nome inexistente foi definido la em cima nos codigos php?>">
+                        <input type="text" name="pesquisa" placeholder="Resultado" class="form-control" id="input5" readonly value="<?php echo isset($cliente['placa']) ? $cliente['placa'] : '' ; //se o nome foi setado o nome vai imprimir se nao vai imprimir nome inexistente, o nome inexistente foi definido la em cima nos codigos php?>">
                     </div>
                     <!-- o botão tera esse tamanho quando começar a diminuir a tela -->
                     <div class="col-sm-4 col-md-2 col-lg-5">
