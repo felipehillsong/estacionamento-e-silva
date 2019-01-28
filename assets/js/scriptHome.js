ce4f36114ed2990f796b0466945c96fc81750246
Felipe Silva <feliperochasilva1987@gmail.com>
2019-01-28 02:02:21 -0200

    conferir se esta certo as datas de cadastro-servico

--- assets/js/scriptHome.js
+++ assets/js/scriptHome.js
@@ -45,25 +45,15 @@ $(document).ready(function()
         if($(this).val() == "Diarista")
         {
             $("#servicos").show();
-            var today = new Date();
-            var dy = today.getDate();
-            var mt = today.getMonth()+1;
-            var yr = today.getFullYear();
-            document.getElementById('tempo').value= dy+"/"+mt+"/"+yr;
-            function diasNoMesSearch(mes, ano) {
-                let data = new Date(ano, mes, 0);
-                return data.getDate();
-            }            
-            let mesAtual = new Date().getMonth()+1; //getMonth retorna um array dos meses que vai de 0 a 11, onde 0 é Janeiro, e 11 é Dezembro. a função "diasNoMesSearch" quer receber o numero do mes atual certo. por esses motivo tem +1 no final. 
-            let anoAtual = new Date().getFullYear(); //pega o ano completo
-            let diasNoMes = diasNoMesSearch(mesAtual, anoAtual); //chama a função!
-            let diaAtual = new Date().getDate(); //pega o dia atual.
-            if (diaAtual === diasNoMes && mesAtual === 11){  
-               diaAtual = 1; //se tivermos no ultimo dia do mês. vamos setar 1 como dia seguinte!
-               mesAtual = 1;
-            }else{
-               diaAtual += +1; //caso não estejamos no ultimo dia do mês, vamos fazer um incremento de +1.
-            }
+            var dia = new Date();
+            var mes = new Date();
+            dia.setDate(dia.getDate()+1);
+            mes.setMonth(dia.getMonth()+1);
+            dia.getFullYear();
+            
+            
+            document.getElementById('tempo').value=dia.getDate()+"/"+mes.getMonth()+"/"+dia.getFullYear();
+           
             document.getElementById('resultadoTempo').value=diaAtual+"/"+mesAtual+"/"+anoAtual;             
             $("#tempo").show();
             $("#tempo2").show();
